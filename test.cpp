ef2e6884c930563f28667ed086c573d23f00eee3
Global Policy <yciloplabolg@gmail.com>
2017-11-30 21:58:56 +0545

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <tchar.h>
+#include "simwin.h"
+
+void BtnOkClick()
+{
+    MessageBox(0,_T("Ok"),_T(":D"),0);
+}
+
+void BtnCancelClick()
+{
+	MessageBox(0,_T("Cancel"),_T(";)"),0);
+}
+
+int main()
+{
+    HWND hwnd = CreateForm(_T("Simwin Form"),_T("Class1"),500,300);
+    HWND hwndbtnOk = AddButton(hwnd, _T("Ok"), 50, 10, 100, 20);
+    AddButtonEvent(hwndbtnOk,LCLICK,&BtnOkClick);
+    HWND hwndbtnCancel = AddButton(hwnd, _T("Cancel"), 50, 30, 50, 20);
+    AddButtonEvent(hwndbtnCancel,LCLICK,&BtnCancelClick);
+    Engage();
+}
