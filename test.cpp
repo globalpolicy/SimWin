e2ed9e5a115410c7bf48c72bd2a05c109b07f951
Global Policy <yciloplabolg@gmail.com>
2017-12-02 00:10:50 +0545

    Update showing textbox functions
    Since I've switched to using Visual Studio for this project, the file is going to be compliant with the requirements of the MSVC++ compiler.
    This is the reason the former int main() function has been changed to int CALLBACK WinMain(...) since the new linker seems to require a WinMain() function in a windows subsystem-based linking. We could continue using int main() by specifying a console subsystem but that would necessarily spawn a console window which we could hide via a ShowWindow but it's not an elegant solution - and the console blink would be clearly visible.
    And the header file is for windows anyway. So, this is how it stands.

--- test.cpp
+++ test.cpp
@@ -1,23 +1,42 @@
-#include <windows.h>
-#include <tchar.h>
+#include "stdafx.h"
 #include "simwin.h"
 
-void BtnOkClick()
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+HWND formhwnd = nullptr;
+HWND btnhwnd = nullptr;
+HWND textboxhwnd = nullptr;
+
+void clicked()
 {
-    MessageBox(0,_T("Ok"),_T(":D"),0);
+	MessageBox(0, _T("Clicked!"), _T(":D"), 0);
 }
 
-void BtnCancelClick()
+void textchanged()
 {
-    MessageBox(0,_T("Cancel"),_T(";)"),0);
+	MessageBox(0, GetTextboxText(textboxhwnd), _T("You typed"), MB_ICONINFORMATION);
 }
 
-int main()
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
 {
-    HWND hwnd = CreateForm(_T("Simwin Form"),_T("Class1"),500,300);
-    HWND hwndbtnOk = AddButton(hwnd, _T("Ok"), 50, 10, 100, 20);
-    AddButtonEvent(hwndbtnOk,LCLICK,&BtnOkClick);
-    HWND hwndbtnCancel = AddButton(hwnd, _T("Cancel"), 50, 30, 50, 20);
-    AddButtonEvent(hwndbtnCancel,LCLICK,&BtnCancelClick);
-    Engage();
+	formhwnd = CreateForm(_T("Title"), _T("SimwinForm"), 300, 500);
+	if (formhwnd) {
+		btnhwnd = AddButton(formhwnd, _T("Ok"), 50, 100, 80, 30);
+		if (btnhwnd) {
+			AddButtonEvent(btnhwnd, BUTTONEVENT_LCLICK, &clicked);
+		}
+		textboxhwnd = AddTextbox(formhwnd, _T("Default text"), 50, 200, 200, 150, false, true, true);
+		if (textboxhwnd) {
+			AddTextboxEvent(textboxhwnd, TEXTBOXEVENT_TEXTCHANGE, &textchanged);
+		}
+		Engage();
+	}
+
 }
